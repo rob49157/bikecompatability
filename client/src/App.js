d9ca2689f1ec61dc2bb742e24586baf9829a7fc3
rob49157 <robertopaipo@hotmail.com>
2023-01-12 21:50:39 -0500

    dropdown

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Menu from "./components/Dropdown"
 
 function App() {
   return (
     <div className="App">
- hello
+      <Menu/>
     </div>
   );
 }
